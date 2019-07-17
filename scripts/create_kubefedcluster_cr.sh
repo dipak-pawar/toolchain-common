073d9c4f01b4aabc755862ef10b3d1b52bab8326
Dipak Pawar <dipakpawar231@gmail.com>
2019-07-17 18:19:38 +0530

    add help function and parameters

--- scripts/create_kubefedcluster_cr.sh
+++ scripts/create_kubefedcluster_cr.sh
@@ -1,14 +1,66 @@
 #!/usr/bin/env bash
 
+user_help () {
+    echo "Creates KubeFedCluster"
+    echo "options:"
+    echo "-t, --type            joining cluster type (host or member)"
+    echo "-mn, --member-ns      namespace where member-operator is running"
+    echo "-hn, --host-ns        namespace where host-operator is running"
+    exit 0
+}
 
-JOINING_CLUSTER_TYPE=$1
-MEMBER_OPERATOR_NS=${MEMBER_NS:toolchain-member-operator}
-HOST_OPERATOR_NS=${HOST_NS:toolchain-host-operator}
-SA_NAME=${JOINING_CLUSTER_TYPE}"-operator"
+if [[ $# -lt 6 ]]
+then
+    user_help
+fi
 
+while test $# -gt 0; do
+       case "$1" in
+            -h|--help)
+                user_help
+                ;;
+            -t|--type)
+                shift
+                JOINING_CLUSTER_TYPE=$1
+                shift
+                ;;
+            -mn|--member-ns)
+                shift
+                if test $# -gt 0; then
+                    MEMBER_OPERATOR_NS=$1
+                    INSTALL_SPECIFIC_VERSION="1"
+                else
+                    echo "Member Operator Namespace is not specified."
+                    exit 1
+                fi
+                shift
+                ;;
+            -hn|--host-ns)
+                shift
+                if test $# -gt 0; then
+                    HOST_OPERATOR_NS=$1
+                else
+                    echo "Host Operator Namespace is not specified."
+                    exit 1
+                fi
+                shift
+                ;;
+            *)
+               echo "$1 is not a recognized flag!"
+               user_help
+               exit -1
+               ;;
+      esac
+done
+
+
+echo "Joining Cluster Type: ${JOINING_CLUSTER_TYPE}"
 echo "Member Operator Namespace: ${MEMBER_OPERATOR_NS}"
 echo "Host Operator Namespace: ${HOST_OPERATOR_NS}"
 
+SA_NAME=${JOINING_CLUSTER_TYPE}"-operator"
+
+# find ns to get secret and create reference secret
 CLUSTER_JOIN_TO="host"
 NS_TO_GET_SECRET="${MEMBER_OPERATOR_NS}"
 NS_TO_CREATE_SECRET="${HOST_OPERATOR_NS}"
@@ -18,6 +70,7 @@ if [[ ${JOINING_CLUSTER_TYPE} == "host" ]]; then
   NS_TO_CREATE_SECRET="${MEMBER_OPERATOR_NS}"
 fi
 
+# getting sa with required secrets and certs
 echo "Switching to project ${NS_TO_GET_SECRET}"
 oc project "${NS_TO_GET_SECRET}"
 
@@ -34,6 +87,8 @@ echo "Switching to project ${NS_TO_CREATE_SECRET}"
 oc project "${NS_TO_CREATE_SECRET}"
 
 CLUSTER_JOIN_TO_NAME=`oc get infrastructure cluster --template={{.status.infrastructureName}}`
+
+# create secret reference on the cluster
 oc create secret generic ${SA_NAME}-${JOINING_CLUSTER_NAME} --from-literal=token="${SA_TOKEN}" --from-literal=ca.crt="${SA_CA_CRT}"
 
 KUBEFEDCLUSTER_CR="apiVersion: core.kubefed.k8s.io/v1beta1
